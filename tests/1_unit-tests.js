83b35561fda22b51f9f3cdd792fd497b8afbd2a9
Nematjon <frank.sport.93@gmail.com>
2021-02-12 12:29:22 +0300

    Add unit tests file

---
+++ tests/1_unit-tests.js
@@ -0,0 +1,10 @@
+const chai = require('chai');
+const assert = chai.assert;
+
+const Solver = require('../controllers/sudoku-solver.js');
+let solver = new Solver();
+
+const puzzleStrings = require('../controllers/puzzle-strings.js');
+
+suite('UnitTests', () => {
+};
