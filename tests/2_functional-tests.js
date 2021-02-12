0105de4b7184704ceca0c0418ed8570e185519e5
Nematjon <frank.sport.93@gmail.com>
2021-02-12 12:32:02 +0300

    Add functional tests

---
+++ tests/2_functional-tests.js
@@ -0,0 +1,10 @@
+const chai = require("chai");
+const chaiHttp = require('chai-http');
+const assert = chai.assert;
+const server = require('../server');
+const puzzleStrings = require('../controllers/puzzle-strings.js');
+
+chai.use(chaiHttp);
+
+suite('Functional Tests', () => {
+});
